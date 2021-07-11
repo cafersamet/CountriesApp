5739501564e553af8d30e370cd28e1704b737593
Cafer Samet Güllüce <cafersamet25@gmail.com>
2021-07-11 16:02:20 +0300

    Initial commit

---
+++ app/src/main/java/com/smart/countriesapp/view/FeedFragment.kt
@@ -0,0 +1,35 @@
+package com.smart.countriesapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.smart.countriesapp.R
+import kotlinx.android.synthetic.main.fragment_feed.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FeedFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FeedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        feed_button.setOnClickListener {
+            val action = FeedFragmentDirections.actionFeedFragment2ToCountryFragment2()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
---
+++ app/src/main/java/com/smart/countriesapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.smart.countriesapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smart.countriesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
